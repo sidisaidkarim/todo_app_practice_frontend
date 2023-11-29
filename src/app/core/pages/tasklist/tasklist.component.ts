0a523070e832234382ebed2f863edcbf7bdeb890
sskarim85@gmail.com <sskarim85@gmail.com>
2023-11-29 15:01:31 +0100

    configured proxy

--- src/app/core/pages/tasklist/tasklist.component.ts
+++ src/app/core/pages/tasklist/tasklist.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class TasklistComponent {
 
+
+  // to be removed, just an example of how to use url in services
+  // since now we are using a proxy server, we don't call http://localhost/api/.. 
+  //we call directly /api/todo/
+  ngOnInit() {
+    fetch("/api/todo/")
+    .then(result=>result.json())
+    .then(data=>console.log(data))
+
+  }
+
 }
